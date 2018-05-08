d743ed79b1e67c1307af9bbc169f4b4fcde60ec2
Giampiero Specogna <giampi_12@hotmail.com>
2018-05-08 19:11:31 -0400

    Initial commit

---
+++ RentACar/Form1.cs
@@ -0,0 +1,17 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace RentACar
+{
+    public partial class LoginForm : MaterialForm
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+    }
+}
